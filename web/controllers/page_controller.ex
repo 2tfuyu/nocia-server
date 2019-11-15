a79e0f02cd0ba713501c5d74aae70c78cc495a05
github nitf <2tf2003@gmail.com>
2019-11-15 10:55:12 +0900

    add: send push message

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -4,4 +4,28 @@ defmodule NociaServer.PageController do
   def index(conn, _params) do
     render conn, "index.html"
   end
+
+  def push(conn, %{"message" => message}) do
+    url = "https://fcm.googleapis.com/fcm/send"
+    api_key = ""
+    registration_ids = [""]
+
+    headers = [{"Content-Type", "application/json"},
+      {"Authorization", "key=#{api_key}"}]
+
+    json_data = %{collapse_key: "ff1",
+                  delay_while_idle: true,
+                  time_to_live: 864000,
+                  notification: %{title: "nocia", body: message},
+                  registration_ids: registration_ids} |> Poison.encode!
+
+    response = HTTPoison.post!(url,  json_data, headers)
+
+    # IO.puts response.body
+
+    case response do
+      %{status_code: 200, body: body} -> json conn, Poison.decode!(body)
+      %{status_code: code} -> json conn, %{error: code}
+    end
+  end
 end
